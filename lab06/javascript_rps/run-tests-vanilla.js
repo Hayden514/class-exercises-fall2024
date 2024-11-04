20b9270be6f35a3b6dbc7ed02648cbd1f86a7c45
Zejun Chen <c8284186609@gmail.com>
2024-11-04 12:46:29 -0500

    Completed Mocha tests and linting setup

--- lab06/javascript_rps/run-tests-vanilla.js
+++ lab06/javascript_rps/run-tests-vanilla.js
@@ -4,26 +4,54 @@ import { assertPrint, runAllTests } from "./helpers.js";
 function testHelloWorld() {
     return assertPrint(
         helloWorld() === "Hello world!",
-        'it returns "Hello world!"'
+        'it returns "Hello world!"',
     );
 }
 
 function testPaperBeatsRock() {
     return assertPrint(
         rps("rock", "paper") === "Paper wins!",
-        "paper beats rock"
+        "paper beats rock",
     );
 }
 
 function testPaperBeatsRockCommutes() {
     return assertPrint(
         rps("paper", "rock") === "Paper wins!",
-        "paper beats rock (flipped)"
+        "paper beats rock (flipped)",
     );
 }
 
 // add more test functions here to exhaustively test your rps function...
+function testScissorsBeatsPaper() {
+    return assertPrint(
+        rps("scissors", "paper") === "Scissors wins!",
+        "scissors beats paper",
+    );
+}
 
+function testRockBeatsScissors() {
+    return assertPrint(
+        rps("rock", "scissors") === "Rock wins!",
+        "rock beats scissors",
+    );
+}
+
+function testTie() {
+    return assertPrint(rps("rock", "rock") === "Tie!", "rock ties with rock");
+}
+
+function testInvalidInput() {
+    return assertPrint(rps("rock", "banana") === "Invalid", "invalid input");
+}
 // Once you have defined each test function, don't forget to
 // add the function definition to the test harness:
-runAllTests([testHelloWorld, testPaperBeatsRock, testPaperBeatsRockCommutes]);
+runAllTests([
+    testHelloWorld,
+    testPaperBeatsRock,
+    testPaperBeatsRockCommutes,
+    testScissorsBeatsPaper,
+    testRockBeatsScissors,
+    testTie,
+    testInvalidInput,
+]);
